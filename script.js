c94b40280034ff96c2344474352df031af043bec
Logan Vasquez <loganvasquez@Logans-MacBook-Pro.local>
2023-09-09 12:42:28 -0500

    Fourth commit

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function redirectTo() {
+document.addEventListener('DOMContentLoaded', function() {
+    // Set the title, description, and link based on URL parameters
+    const title = getParameterByName('title');
+    const description = getParameterByName('description');
     const link = getParameterByName('link');
-    if (link) {
-        window.open(link, '_blank');
+
+    if (title) {
+        document.querySelector('h2').innerText = title;
     }
-}
+
+    if (description) {
+        document.querySelector('p').innerText = description;
+    }
+
+    if (link) {
+        document.querySelector('button').addEventListener('click', function() {
+            window.open(link, '_blank');
+        });
+    }
+});
